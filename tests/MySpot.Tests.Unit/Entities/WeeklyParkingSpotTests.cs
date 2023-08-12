004e3a3ab98e6403f6d4d8341227e9332cf8748a
MafistoPL <mateusz.poreba@outlook.com>
2023-08-12 11:23:01 +0200

    test(reservations): move common initialization to ctor, add new test
    Moved common tests initialization logic to the constructor to enhance code reusability. Added a new test `given_reservation_for_already_existing_date_add_reservation_should_fail()` to ensure that adding a reservation for an already existing date fails as expected.

--- tests/MySpot.Tests.Unit/Entities/WeeklyParkingSpotTests.cs
+++ tests/MySpot.Tests.Unit/Entities/WeeklyParkingSpotTests.cs
@@ -9,27 +9,55 @@ namespace MySpot.Tests.Unit.Entities;
 
 public class WeeklyParkingSpotTests
 {
+    #region Arrange
+
+    private readonly Date _now;
+    private readonly WeeklyParkingSpot _weeklyParkingSpot;
+
     public WeeklyParkingSpotTests()
     {
-        
+        _now = new Date(new DateTime(2022, 08, 10));
+        _weeklyParkingSpot = new WeeklyParkingSpot(Guid.NewGuid(), new Week(_now), "P1");
     }
-    
-    [Fact]
-    public void given_invalid_date_add_reservation_should_fail()
+
+    #endregion
+
+    [Theory]
+    [InlineData("2022-08-09")]
+    [InlineData("2022-08-17")]
+    public void given_invalid_date_add_reservation_should_fail(string dateString)
     {
         // ARRANGE
-        var now = new DateTime(2022, 08, 10);
-        var invalidDate = now.AddDays(7);
-        var weeklyParkingSpot = new WeeklyParkingSpot(Guid.NewGuid(), new Week(now), "P1");
-        var reservation = new Reservation(Guid.NewGuid(), weeklyParkingSpot.Id, 
+        var invalidDate = DateTime.Parse(dateString);
+        var reservation = new Reservation(Guid.NewGuid(), _weeklyParkingSpot.Id, 
             "John Doe", "XYZ123", new Date(invalidDate));
 
         // ACT
         var exception = Record.Exception(
-            () => weeklyParkingSpot.AddReservation(reservation, new Date(now)));
+            () => _weeklyParkingSpot.AddReservation(reservation, _now));
 
         // ASSERT
         exception.ShouldNotBeNull();
         exception.ShouldBeOfType<InvalidReservationDateException>();
+    }
+
+    [Fact]
+    public void given_reservation_for_already_existing_date_add_reservation_should_fail()
+    {
+        // ARRANGE
+        var reservationDate = _now.AddDays(1);
+        var reservation = new Reservation(Guid.NewGuid(), _weeklyParkingSpot.Id, 
+            "John Doe", "XYZ123", reservationDate);
+        var nextReservation = new Reservation(Guid.NewGuid(), _weeklyParkingSpot.Id, 
+            "John Doe", "XYZ123", reservationDate);
+        _weeklyParkingSpot.AddReservation(reservation, _now);
+        
+        // ACT
+        var exception = Record.Exception(
+            () => _weeklyParkingSpot.AddReservation(reservation, _now));
+        
+        // ASSERT
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<ParkingSpotAlreadyReservedException>();
     }
 }
