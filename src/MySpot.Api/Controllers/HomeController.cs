03783c4103172243a5df4cb49b08c6cc820f4a00
MafistoPL <mateusz.poreba@outlook.com>
2023-08-13 22:41:56 +0200

    feat(appsettings): get appsettings in home controller

---
+++ src/MySpot.Api/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace MySpot.Api.Controllers;
+
+[Route("")]
+public class HomeController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+
+    public HomeController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet]
+    public ActionResult<string> Get() => _configuration["name"];
+}
